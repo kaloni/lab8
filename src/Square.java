5359060f0df158380c3bf6eeccac98569d6a126b
perezsam <escalantesammy@gmail.com>
2014-12-19 03:00:31 +0800

    Square done

--- src/Square.java
+++ src/Square.java
@@ -9,8 +9,8 @@ public class Square extends Shape {
 	
 	@Override
 	public double getArea() {
-		// TODO Auto-generated method stub
-		return 0;
+		double area = side*side; 
+		return area;
 	}
 
 }
